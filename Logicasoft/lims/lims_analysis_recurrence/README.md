b3646ffde5ff8f16d02f57267468902794b5b93b
Your Name <you@example.com>
2024-03-23 03:47:27 +0400

    ..

--- Logicasoft/lims/lims_analysis_recurrence/README.md
+++
@@ -1,4 +0,0 @@
-# TODO list
-- When duplicated : Fill with the origin_analysis into parent_analysis.
-- When duplicated : Fill is_duplicate in analysis to True.
-- If Lims_samples implemented, add duplication method for lims.sample.
--- Logicasoft/lims/lims_decimal_dynamic/static/src/js/fields/field_registry.js
+++
@@ -1,9 +0,0 @@
-odoo.define('lims_decimal_dynamic.field_registry', function (require) {
-"use strict";
-
-var registry = require('web.field_registry');
-var relationalFields = require('lims_decimal_dynamic.relational_fields');
-
-registry.add('float_dynamic', relationalFields.FieldFloatDynamic);
-
-});
